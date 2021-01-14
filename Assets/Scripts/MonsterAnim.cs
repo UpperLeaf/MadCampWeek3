795dce9592366dcf37b9b58d31ca1b9342396b2e
retroinspect <sangachoi2103@gmail.com>
2021-01-14 19:54:57 +0900

    몬스터 스프라이트 및 이동 애니메이션 추가. ray로 방향 전환하도록 수정.

---
+++ Assets/Scripts/MonsterAnim.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterAnim : MonoBehaviour
+{
+    Rigidbody2D rigid;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rigid.velocity = new Vector2(1, rigid.velocity.y); // y 값으로 0을 넣으면 안됨!
+
+    }
+}
