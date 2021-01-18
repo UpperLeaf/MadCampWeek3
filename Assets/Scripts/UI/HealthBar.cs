6941110f54e0a6a5d99cdcd4500609c522055321
UpperLeaf <tkdduq8260@gmail.com>
2021-01-18 21:42:31 +0900

    Fixed Health Bar Issue

--- Assets/Scripts/UI/HealthBar.cs
+++ Assets/Scripts/UI/HealthBar.cs
@@ -8,15 +8,17 @@ public class HealthBar : MonoBehaviour
 {
     // PlayerController 내부의 hitPoints 주소를 저장하게 되는 변수
     // Player와 데이터를 공유하기 위해 만듦
+    [SerializeField]
     public HitPoints hitPoints;
 
     // Player의 maxHitPoints 값을 얻기 위해 현재 Player 오브젝트 필요
     // 현재 Player 오브젝트의 주소를 저장하는 변수
-    [HideInInspector]
+    [SerializeField]
     public Player player; // 
 
     public Image meterImage;
 
+    [SerializeField]
     float maxHitPoints;
 
     // Start is called before the first frame update
@@ -28,6 +30,7 @@ public class HealthBar : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(hitPoints.HP);
         StartCoroutine("HpBarUpdate");
     }
 
